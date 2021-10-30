f38029d8bd17a906c2f78c77bed10b0a2bb52948
FuJing0316 <582473443@qq.com>
2021-10-30 16:06:58 +0800

    file api practice

--- src/io/FileDemo.java
+++ src/io/FileDemo.java
@@ -44,10 +44,64 @@ public class FileDemo {
             e.printStackTrace();
         }
 
+        //无论文件是否存在，都可以获得文件的路径
+//        File  file1 = new File("c:/a.txt");
+        File file1 = new File("abc.txt");
+        System.out.println(file1.getAbsolutePath());
+
+        //判断是文件还是目录
+        System.out.println(file1.isDirectory());
+        System.out.println(file1.isFile());
 
 
+        //File.listFiles() 用的比较多
+        File file2 = new File("c:/f/g/h");
+        String[] list = file2.list();
+/*        for (String s : list) {
+            System.out.println(s);
+        }
+        System.out.println("-----------------");*/
+        File[] files = file2.listFiles();
+/*        for (File f : files) {
+            System.out.println(f);
+        }*/
+
+        //打印当前系统的所有盘符
+        File[] fs = File.listRoots();
+        for (File f : fs) {
+            System.out.println(f);
+        }
+
+        //创建单级目录
+        file2.mkdir();
+        //创建多级目录
+        file2.mkdirs();
 
 
+        /**
+         * 问题： 循环遍历输出 c:/d/ 目录下的所有文件的绝对路径
+         *       递归实现（因为不知道循环多少次，所以无法循环遍历输出。但是，一般递归要慎用，因为递归如果没有合适的出口的话，很容易超出栈的最大深度）
+         */
+        printFile(new File("C:\\D\\00--面试\\00-我的简历"));
+
+
+    }
+
+
+    /**
+     * 注意点：文件在遍历的时候，有时候会报空指针，原因在于当前文件受系统保护，没有访问权限，此时会报空指针
+     *
+     * @param file
+     */
+    private static void printFile(File file) {
+        if (file.isFile()) {
+            System.out.println("这是一个具体文件，只有文件名称：" + file.getAbsolutePath());
+        } else if (file.isDirectory()) {
+            File[] files = file.listFiles();
+            for (File f : files) {
+                printFile(f);
+            }
+        }
     }
 
 }
