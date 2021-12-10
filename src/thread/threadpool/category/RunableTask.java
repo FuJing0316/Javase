572d152637f2ee3d42036be73c27d8c8c04edb8b
FuJing0316 <582473443@qq.com>
2021-12-10 20:03:41 +0800

    1、newCachedThreadPool

--- src/thread/threadpool/category/RunableTask.java
+++ src/thread/threadpool/category/RunableTask.java
@@ -7,14 +7,18 @@ package thread.threadpool.category;
  * @Version: 1.0
  */
 public class RunableTask implements Runnable {
+
     @Override
     public void run() {
-
-//        try {
-//            Thread.sleep(1000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
+        /**
+         * 此处的睡眠： 是为了看清线程是否复用
+         * 每个线程执行执行run()方法消耗的时间越长，线程池中空闲线程就越少（每个任务都需要创建新线程去执行任务）
+         */
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         System.out.println(Thread.currentThread().getName() + " running");
     }
