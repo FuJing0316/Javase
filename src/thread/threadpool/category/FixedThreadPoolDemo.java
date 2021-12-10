8687dee095d294b6df9a5f5c95606fad5c9461e3
FuJing0316 <582473443@qq.com>
2021-12-10 21:36:38 +0800

    1、FixedThreadPool

---
+++ src/thread/threadpool/category/FixedThreadPoolDemo.java
@@ -0,0 +1,13 @@
+package thread.threadpool.category;
+
+/**
+ * @Author: fujing
+ * @Date: 2021/12/10
+ * @Description:
+ * @Version: 1.0
+ */
+public class FixedThreadPoolDemo {
+    public static void main(String[] args) {
+
+    }
+}
