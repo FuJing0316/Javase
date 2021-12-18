999956dcc479e1f3f8e4374928fac766dd90662e
FuJing0316 <582473443@qq.com>
2021-12-18 16:03:39 +0800

    java net:socket通信2,client and Server双端通信

--- src/net/server/Server.java
+++ src/net/server/Server.java
@@ -14,7 +14,7 @@ import java.net.Socket;
  */
 public class Server {
     public static void main(String[] args) throws IOException {
-        //服务端需要使用serverSocket需要开发自己本地的端口
+        //服务端需要使用serverSocket来开启自己本地的端口
         ServerSocket serverSocket = new ServerSocket(10086);
         //要接收客户端传过来的数据，需要定义一个socket对象
         Socket sever = serverSocket.accept();
