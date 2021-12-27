7e21e9dc266ed73b40a240be40f950230b3cd9ec
FuJing0316 <582473443@qq.com>
2021-12-27 21:28:23 +0800

    lambda demo:任何 用函数式接口 的地方，均可用lambda表达式

--- src/lambda/LambdaDemo.java
+++ src/lambda/LambdaDemo.java
@@ -10,7 +10,12 @@ package lambda;
 /**
  * lambda表达式的使用场景：任何 有 函数式接口 的地方
  *
- * 什么是函数式接口？---只有一个抽象方法的接口（Object类中的方法除外）
+ * 什么是函数式接口？---只有一个抽象方法的接口（Object类中的方法除外），一般会使用注解 @FunctionalInterface 标记
+ *
+ * 使用lambda的好处？
+ *  1、替代 只有一个方法的接口实现，告别匿名内部类，使代码更简洁；
+ *  2、lambda表达式还提升了对集合
+ *
  *
  */
 public class LambdaDemo {
@@ -24,11 +29,19 @@ public class LambdaDemo {
         });
         thread.start();
 
-
         //方式二：lambda表达式
         new Thread(()->{System.out.println("lambda running...");}).start();
 
         //lambda简写
         new Thread(()-> System.out.println("简化lambda写法 running。。。。")).start();
+
+
+
+
+
+
+
+
+
     }
 }
