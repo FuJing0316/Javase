8206f8e5976a3f12eca157ffaa370ce66b2bc140
FuJing0316 <582473443@qq.com>
2022-01-05 20:39:06 +0800

    lambda表达式的写法 ： 根据函数式接口中，抽象方法参数的个数，以及实现逻辑，会有的变种写法

---
+++ src/lambda/lambda_exec/LambdaTest2.java
@@ -0,0 +1,13 @@
+package lambda.lambda_exec;
+
+/**
+ * @Author: fujing
+ * @Date: 2022/1/5
+ * @Description:
+ * @Version: 1.0
+ */
+public class LambdaTest2 {
+    public static void main(String[] args) {
+
+    }
+}
