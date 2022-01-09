6131d7cd0e2a961fa1a11ea5b0ef4b9db8514c72
FuJing0316 <582473443@qq.com>
2022-01-09 20:53:31 +0800

    Stream Api--stream的创建

---
+++ src/lambda/stream/StreamDemo.java
@@ -0,0 +1,29 @@
+package lambda.stream;
+
+/**
+ * @Author: fujing
+ * @Date: 2022/1/9
+ * @Description:
+ * @Version: 1.0
+ */
+
+/**
+ * Stream Api:
+ *      Stream的创建
+ *
+ *
+ *      stream的中间操作
+ *
+ *
+ *      stream的终止操作
+ *
+ *
+ */
+public class StreamDemo {
+    public static void main(String[] args) {
+        //-----------------stream的创建-------------------
+
+
+
+    }
+}
