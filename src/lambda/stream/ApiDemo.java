4b75c44fd51daacd5cfe424b01426ba9b8d1a5d9
FuJing0316 <582473443@qq.com>
2022-01-15 22:49:59 +0800

    stream 终止操作 api

---
+++ src/lambda/stream/ApiDemo.java
@@ -0,0 +1,98 @@
+package lambda.stream;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * @Author: fujing
+ * @Date: 2022/1/15
+ * @Description:
+ * @Version: 1.0
+ */
+public class ApiDemo {
+    class Student{
+        private int id;
+        private String name;
+        private int age;
+        public Student(int id,String name,int age){
+            this.id = id;
+            this.name = name;
+            this.age = age;
+        }
+        public int getId() {
+            return id;
+        }
+        public String getName() {
+            return name;
+        }
+        @Override
+        public String toString() {
+            return "Student{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    ", age=" + age +
+                    '}';
+        }
+    }
+
+    public static void main(String[] args) {
+        ApiDemo demo1 = new ApiDemo ();
+        List<Student> list = new ArrayList<>();
+        list.add (demo1.new Student (1,"张三",18));
+        list.add (demo1.new Student (2,"李四",20));
+        list.add (demo1.new Student (3,"王二",22));
+        list.add (demo1.new Student (4,"小明",19));
+        list.add (demo1.new Student (5,"小红",17));
+        list.add (null);
+        //常用操作一：防空处理
+        //Optional.ofNullable (list).orElse (Collections.emptyList ()).forEach (System.out::println);  会存在null
+        list.stream ().filter (Objects::nonNull).forEach (student -> System.out.println (student));
+        //常用操作二：集合变形  业务代码最为常见（假设我要取得某个集合中某个属性作为单独的一个集合）
+        System.out.println ("=======================集合变形分割线===================");
+        List<String> nameList = list.stream ().filter (Objects::nonNull).map (Student::getName).collect(Collectors.toList());
+        nameList.forEach (System.out::println);//集合转化流--->filter过滤--->map对集合做业务操作(返回的仍然是Stream<R>)--->流转化为集合
+        //limit 就像mysql的limit num 关键字  这里是查出前3条
+        List<String> nameList2 = list.stream ().filter (Objects::nonNull).map (Student::getName).limit (3).collect(Collectors.toList());
+        nameList2.forEach (s -> System.out.println ("limit" + s));
+        //加上skip 类似于mysql分页 比如查看第1 到 10 条记录 select * from emp limit 0，9     这里也类似：这就是第2到第4条   也可以用于分页
+        List<String> nameList3 = list.stream ().filter (Objects::nonNull).map (Student::getName).skip (1).limit (3).collect(Collectors.toList());
+        nameList3.forEach (s -> System.out.println ("skip  limit" + s));
+        List<Integer> list2 = Arrays.asList(1,2,3);
+        //不改变原有元素
+        list2.forEach(i -> i = i*2);
+        list2.forEach(integer -> System.out.println ("不改变原有元素"+integer));
+        //改变对象
+        list2.stream().map(i -> i * 2).forEach(integer -> System.out.println ("改变原有元素"+integer));
+        //常用操作三：list转map
+        System.out.println ("=======================list转map分割线===================");
+        Map<Integer,Student> map = list.stream ().filter (Objects::nonNull).collect (Collectors.toMap (Student::getId, student -> student));
+        for (Map.Entry<Integer, ApiDemo.Student> entrySet: map.entrySet ()){
+            System.out.println (entrySet.getKey () +":"+ entrySet.getValue ());
+        }
+        System.out.println ("=======================排序后分割线===================");
+        //常用操作四：集合排序
+        //方法1：利用Comparable 接口实现排序 String和Integer都是实现了此接口，需要覆写其
+        List<Student> notNullList = list.stream ().filter (Objects::nonNull).collect (Collectors.toList ());
+        Collections.sort (notNullList,(o1, o2) -> {
+            return Integer.compare (o1.age,o2.age);
+        });
+        //利用Comparator 接口实现排序
+        Collections.sort (notNullList,Comparator.comparing (o -> o.age));
+        notNullList.forEach (student -> System.out.println (student));
+
+        //当然觉得性能差 可以用并行方式输出，但结果不一定是顺序执行 1234...9
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        numbers.parallelStream().forEach(System.out::println);
+        numbers.parallelStream ().forEachOrdered (System.out::println);//顺序执行
+
+        //list 转map
+//        Map<String, List<CollegeMajorEnum>> majorEnumMap = userService.getMajorEnum().stream().collect(Collectors.groupingBy(CollegeMajorEnum::getMajorCode));
+        //对list某个变量进行+1
+//        List<HqQuestionnairePunch> collect = list.stream().map(hqQuestionnairePunch -> hqQuestionnairePunch.setPunchTimes(hqQuestionnairePunch.getPunchTimes() + 1)).collect(Collectors.toList());
+        //对某个list对象取出某个属性
+//        List<Integer> exists = list.stream().map(HqQuestionnairePunch::getUid).collect(Collectors.toList());
+
+
+    }
+
+}
--- src/lambda/stream/StreamDemo.java
+++ src/lambda/stream/StreamDemo.java
@@ -8,32 +8,39 @@ package lambda.stream;
  */
 
 import java.math.BigDecimal;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 /**
  * Stream Api:
- *      Stream的创建/生成
- *          1、Stream.of(T..values)  返回元素是指定值的顺序排序流
- *          2、list.stream()
- *          3、Arrays.stream(T[] array)
- *          4、Stream.generate(Supplier<T> s)
- *          5、iterate(final T seed, final UnaryOperator<T> f)
- *          6、其他方式。。。
- *
- *
- *      stream的中间操作 : 如果方法调用之后，返回的结果是 Stream 对象，就意味着是一个中间操作
- *          常用中间操作：
- *              map、flatMap、filter、limit、skip、distinct、sorted()、sorted(Comparator<? super T> comparator)
- *
- *
- *
- *      stream的终止操作：
- *          1、
- *
+ * Stream的创建/生成
+ * 1、Stream.of(T..values)  返回元素是指定值的顺序排序流
+ * 2、list.stream()
+ * 3、Arrays.stream(T[] array)
+ * 4、Stream.generate(Supplier<T> s)
+ * 5、iterate(final T seed, final UnaryOperator<T> f)
+ * 6、其他方式。。。
+ * <p>
+ * <p>
+ * stream的中间操作 : 如果方法调用之后，返回的结果是 Stream 对象，就意味着是一个中间操作
+ * 常用中间操作：
+ * map、flatMap、filter、limit、skip、distinct、sorted()、sorted(Comparator<? super T> comparator)
+ * <p>
+ * stream的终止操作：
+ * reduce （归约）：将流中元素反复结合起来得到一个值
+ * collect（收集）：将流转换为其他形式。需要Collectors类的一些方法
+ * <p>
+ * allMatch
+ * anyMatch
+ * noneMatch
+ * findFirst
+ * findAny
+ * count()
+ * max()
+ * min()
  */
 public class StreamDemo {
     public static void main(String[] args) {
@@ -47,7 +54,7 @@ public class StreamDemo {
 
 //---------------------------Stream的中间操作----------------------
         Stream<Integer> stream1 = Arrays.asList(1, 2, 2, 3, 3, 4, 5, 5, 6, 6, 7, 8, 9, 10, 10, 10).stream();
-/*        stream1.map((x)-> x+1) //代表一个映射， 返回 把给定函数 应用于此流的元素之后 所得到的结果 组成的流
+        stream1.map((x) -> x + 1) //代表一个映射， 返回 把给定函数 应用于此流的元素之后 所得到的结果 组成的流
                 .flatMap(t -> Stream.of(t)) //和 map 差不多. 区别类似 list.add 和list.addAll
                 .filter(s -> s <= 5) //按指定函数（方法实现逻辑）过滤
                 .limit(5) //限制
@@ -55,10 +62,10 @@ public class StreamDemo {
                 .distinct() //去重
                 .sorted() //自然排序
                 .sorted(Integer::compareTo) //自定义排序
-                .forEach(System.out::println); //最后一步，是终止操作 ，循环输出 前几步处理过的 流中元素*/
+                .forEach(System.out::println); //最后一步，是终止操作 ，循环输出 前几步处理过的 流中元素
 
         //api分步使用
-        Stream<Integer> stream = Arrays.asList(11,12,13,14,15,15,16,16,17,18,18).stream();
+        Stream<Integer> stream = Arrays.asList(11, 12, 13, 14, 15, 15, 16, 16, 17, 18, 18).stream();
 
         //map(Function<? super T, ? extends R> mapper) ： 返回把给定函数 应用于此流的元素 之后所得到的结果 组成的流
 /*        Stream<Integer> integerStream = stream.map(x -> {
@@ -72,17 +79,76 @@ public class StreamDemo {
 
         //统计集合中偶数的个数
         Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9).stream().filter((x) -> x % 2 == 0).mapToInt(x -> x).forEach(System.out::print);
+        System.out.println("\n" + "-------------------------");
         long count = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9).stream().filter((x) -> x % 2 == 0).mapToInt(x -> x).count();
         System.out.println(count);
 
+        //计算集合中偶数之和
+        System.out.println("-------------------------");
+        int sum = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9).stream().filter((x) -> x % 2 == 0).mapToInt(x -> x).sum();
+        System.out.println(sum);
+
+        //集合元素--每个加1--去重--求和
+        System.out.println("-------------------------");
+        int sum1 = Stream.of(1, 2, 2, 5, 5, 6, 7).mapToInt(x -> x + 1).distinct().sum(); //mapToInt  将集合中每个元素映射成 int，返回一个stream
+        System.out.println("sum1:" + sum1);
 
 
+//---------------------------Stream的终止操作----------------------
+        /**
+         * 常用终止操作 Api
+         */
+        Stream<Integer> stream2 = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        System.out.println(stream2.allMatch((x) -> x == 10));  // 检查是否匹配所有元素 boolean
+        System.out.println(stream2.anyMatch(((x) -> x > 5))); // 检查是否至少匹配一个元素
+        System.out.println(stream2.noneMatch((x) -> x > 500)); //检查是否没有匹配所有元素
+        System.out.println(stream2.findFirst()); // 返回第一个元素Optional[1]
+        System.out.println(stream2.findAny()); // 返回当前流中的任意一个元素;
+        System.out.println(stream2.count()); // 返回流中元素的总个数);
+        Optional<Integer> max = stream2.max(Integer::compareTo); // 返回流中最大值
+        System.out.println("max " + max.get());
+        Optional<Integer> min = stream2.min(Integer::compareTo);//返回流中最小值
+        System.out.println("min " + min.get());
+
+
+
+        /**
+         * reduce （归约）：将流中元素反复结合起来得到一个值
+         */
+        Integer reduce = stream2.map(s -> (s + 1)).reduce(0, (integer1, integer2) -> integer1 + integer2);
+        //归约：0为第一个参数x的默认值，x是计算后的返回值，y为每一项的值。
+        System.out.println(reduce);
+        Optional<Integer> reduce1 = stream2.map(s -> (s + 1)).reduce((x, y) -> x + y);
+        // x是计算后的返回值，默认为第一项的值，y为其后每一项的值。
+        System.out.println(reduce1.get());
+        //求和
+//        BigDecimal bigDecimal = orderInfos.stream().map(OrderInfoReq::getMoney).reduce(BigDecimal::add).get();
+
+
+        /**
+         * collect（收集）：将流转换为其他形式。需要Collectors类的一些方法
+         */
+        Set<Integer> collect = stream2.collect(Collectors.toSet());
+        List<Integer> collect2 = stream2.collect(Collectors.toList());
+        HashSet<Integer> collect1 = stream2.collect(Collectors.toCollection(HashSet::new));
+        //分组 {group=[1, 2, 3, 4...]}
+        Map<String, List<Integer>> collect3 = stream2.collect(Collectors.groupingBy((x) -> "group"));//将返回值相同的进行分组
+        System.out.println("collect3--分组:" + collect3);
+
+        //分区 {false=[1, 2, 3, 4], true=[5, 6, 7, 8, 9, 10, 10]}
+        Map<Boolean, List<Integer>> collect5 = stream2.collect(Collectors.partitioningBy((x) -> x >= 5));
+        System.out.println("collect5:" + collect5);
+
+        //汇总： 最大值、最小值、平均值、个数
+        DoubleSummaryStatistics collect6 = stream2.collect(Collectors.summarizingDouble((x) -> x));
+        System.out.println(collect6.getMax());
+        System.out.println(collect6.getCount());
 
 
     }
 
 
-    private static void gen6(){
+    private static void gen6() {
         String str = "hello ll";
         IntStream chars = str.chars();
         chars.forEach(System.out::println);//输出每个元素对应的int值 （ascii码）
@@ -92,7 +158,7 @@ public class StreamDemo {
     /**
      * 生成 5、iterate(final T seed, final UnaryOperator<T> f)
      */
-    private static void gen5(){
+    private static void gen5() {
         Stream<String> iterate = Stream.iterate("h", (x) -> {
             return x.concat("_stream_test");
         });
@@ -101,11 +167,10 @@ public class StreamDemo {
     }
 
 
-
     /**
      * 生成 4、 Stream.generate(Supplier<T> s)
      */
-    private static void gen4(){
+    private static void gen4() {
         Stream<Integer> generate = Stream.generate(() -> {
             int a = 15;
             return a;
@@ -116,7 +181,7 @@ public class StreamDemo {
     /**
      * 生成 3、Arrays.stream(T[] array)
      */
-    private static  void gen3(){
+    private static void gen3() {
         Stream<String> stream = Arrays.stream(new String[1]);
         stream.forEach(System.out::println);
     }
@@ -124,7 +189,7 @@ public class StreamDemo {
     /**
      * 生成 2、list.stream()
      */
-    private static void gen2(){
+    private static void gen2() {
         List<String> list = Arrays.asList("1", "2", "3", "4");
         Stream<String> stream = list.stream();
         stream.forEach(System.out::print);
