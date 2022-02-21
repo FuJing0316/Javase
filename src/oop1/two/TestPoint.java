c2ba6b79977332780b2d4f536fb349166b0f875e
FuJing0316 <582473443@qq.com>
2022-02-21 21:37:58 +0800

    计算两点之间的具体

---
+++ src/oop1/two/TestPoint.java
@@ -0,0 +1,29 @@
+package oop1.two;
+
+/**
+ * @Author: fujing
+ * @Date: 2022/2/21
+ * @Description:
+ * @Version: 1.0
+ */
+public class TestPoint {
+    public static void main(String[] args) {
+        Point p1 = new Point();
+        p1.setX(3);
+        p1.setY(1);
+
+        Point p2 = new Point();
+        p2.setX(4);
+        p2.setY(5);
+
+        System.out.println("两点之间的距离=="+calDistance(p1, p2));
+
+
+    }
+
+   static double calDistance(Point p1,Point p2){
+        return Math.sqrt((p1.getX() - p2.getX()) * (p1.getX() - p2.getX()) + (p1.getY() - p2.getY())*(p1.getY() - p2.getY()));
+    }
+
+
+}
