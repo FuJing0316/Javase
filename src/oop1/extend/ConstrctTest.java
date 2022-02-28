48105524733acc43bf3a24216129efd3560ac4df
FuJing0316 <582473443@qq.com>
2022-02-28 20:22:44 +0800

    oop

---
+++ src/oop1/extend/ConstrctTest.java
@@ -0,0 +1,30 @@
+package oop1.extend;
+
+/**
+ * @Author: fujing
+ * @Date: 2022/2/27
+ * @Description:
+ * @Version: 1.0
+ */
+
+/**
+ * 构造器是不能子类继承的
+ */
+public class ConstrctTest {
+    public static void main(String[] args) {
+
+    }
+}
+
+class A{
+    public A(){
+
+    }
+}
+
+
+class B extends A{
+    public B(){
+
+    }
+}
