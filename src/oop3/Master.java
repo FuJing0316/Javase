30f92d43c8f5cb00116dee2e89e0c2df24ff2f20
FuJing0316 <582473443@qq.com>
2022-03-07 21:59:49 +0800

    多态出现的前提：1-必须有继承关系（接口实现和接口也属于继承关系）；2-子类必须重写父类方法；3-父类引用指向子类对象

--- src/oop3/Master.java
+++ src/oop3/Master.java
@@ -3,7 +3,7 @@ package oop3;
 /**
  * @Author: fujing
  * @Date: 2022/3/7
- * @Description:   主人：主人陪宠物玩、主人喂宠物
+ * @Description:   主人：1-主人陪宠物玩不同的游戏；2-主人给不同宠物喂食；3-主人领养不同的宠物
  * @Version: 1.0
  */
 public class Master {
